d12be5a9f119f9fb4cd7fe7171cd908cef78fb5d
chon <balint.ducsai@gmail.com>
2023-06-18 21:09:56 +0200

    feat: :lipstick: add TailwindCSS
    Initialize TailwindCSS and update UI accordingly

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-      <div>
+      <div className='flex justify-center'>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -16,9 +16,9 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1 className='font-bold'>Vite + React + <span className="text-cyan-400">TailwindCSS</span></h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button className='mb-3' onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
