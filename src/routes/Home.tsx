2f869ee92fb3ee925aee3e39757809befda8bb8a
chon <balint.ducsai@gmail.com>
2023-07-04 23:11:09 +0200

    feat: add routing
    This commit implements a basic navigation system.  Also lays the foundation of the folder structure.

---
+++ src/routes/Home.tsx
@@ -0,0 +1,3 @@
+export default function Home() {
+  return <h1 className="text-3xl md:text-4xl lg:text-6xl">ernesto-chon</h1>;
+}
---
+++ src/routes/WhoWeAre.tsx
@@ -0,0 +1,3 @@
+export default function WhoWeAre() {
+  return <h1 className="text-3xl md:text-4xl lg:text-6xl">who-we-are</h1>;
+}
