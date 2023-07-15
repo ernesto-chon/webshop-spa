a156da0c4b5a71de2830a65f605d094177f783a5
chon <balint.ducsai@gmail.com>
2023-07-15 21:17:36 +0200

    feat: add example
    Add example component

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,3 +1,10 @@
+import Button from '../components/Button';
+
 export default function Home() {
-  return <h1 className="text-3xl md:text-4xl lg:text-6xl">ernesto-chon</h1>;
+  return (
+    <div>
+      <h1 className="text-3xl md:text-4xl lg:text-6xl">ernesto-chon</h1>
+      <Button>Hello button</Button>
+    </div>
+  );
 }
