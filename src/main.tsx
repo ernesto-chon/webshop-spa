d379914e72ee08d83bc9b2d5ec944263f49598bf
chon <balint.ducsai@gmail.com>
2023-07-16 11:54:32 +0200

    feat: add base typography styles
    Config typography base styles according to the design. Also exmaples has been added to Home route.

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+import '@fontsource/roboto';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import '@fontsource/jost';
+import '@fontsource/jost/500.css';
+import '@fontsource/jost/700.css';
+
 import './index.css';
+
 import Router from './Router';
 
 
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,17 @@ export default {
         primary: '#C7AB62',
         'primary-hover': '#AE9249',
       },
+      fontFamily: {
+        jost: ['Jost', 'sans-serif'],
+        roboto: ['Roboto', 'sans-serif'],
+      },
+      fontWeight: {
+        bold: '500',
+        extrabold: '700',
+      },
     },
   },
+  
   plugins: [daisyui],
   daisyui: {
     styled: false,
