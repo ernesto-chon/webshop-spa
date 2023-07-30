474b078fe9afe11adaa3650b40ad34f195c031fb
chon <balint.ducsai@gmail.com>
2023-07-30 22:23:00 +0200

    fix: button content color
    Button content's color was being override by parent in some cases.

--- src/components/atoms/Button.tsx
+++ src/components/atoms/Button.tsx
@@ -5,7 +5,7 @@ type Props = ButtonHTMLAttributes<HTMLButtonElement>;
 export default function Button({ children, ...props }: Props) {
   return (
     <button
-      className="d-btn rounded-sm  bg-primary text-base transition-colors !duration-300 hover:bg-primary-hover"
+      className="d-btn rounded-sm bg-primary text-base text-white transition-colors !duration-300 hover:bg-primary-hover"
       {...props}
     >
       {children}
