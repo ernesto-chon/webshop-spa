6f2a02d6f5548c8878b230b2176e438f63e679b1
Ernesto-Tz <ivan.tzompantzi96@gmail.com>
2023-08-08 13:46:57 +0200

    fix: Change folder name with typo for "Organism"

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,5 +1,5 @@
 import { Outlet, useLocation } from 'react-router-dom';
-import Navbar from './components/Navbar';
+import Navbar from './components/organism/Navbar';
 import { useMemo } from 'react';
 
 export default function Layout() {
