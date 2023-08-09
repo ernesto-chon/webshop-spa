091494384134db750581e53a6961f8930c152c5d
Ernesto-Tz <ivan.tzompantzi96@gmail.com>
2023-08-09 17:38:51 +0200

    perf: :construction: Implement react hook form on a component
    The SignIn Inputs use hook form instead of useState

--- src/components/Authentication/SignIn.tsx
+++ src/components/Authentication/SignIn.tsx
@@ -1,48 +1,58 @@
 import React, { useState, FormEvent } from 'react';
 import { auth } from '../../firebase';
 import { signInWithEmailAndPassword } from 'firebase/auth';
+import { useForm } from 'react-hook-form';
 
 const SignIn: React.FC = () => {
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
+  // const [email, setEmail] = useState('');
+  // const [password, setPassword] = useState('');
 
-  const signIn = (e: FormEvent) => {
-    e.preventDefault();
-    signInWithEmailAndPassword(auth, email, password)
+  const { register, handleSubmit } = useForm();
+
+  const signIn = (data: any) => { //TODO: change any
+    const newUser = {email: data.userEmail, password: data.userPassword};
+    signInWithEmailAndPassword(auth, newUser.email, newUser.password)
       .then((userCredentials) => {
         console.log(userCredentials)
       })
       .catch((error) => {
         console.log(error)
       })
-  }
+  };
   return (
     <div className="mt-5">
-      <form className="grid grid-cols-1 gap-2" onSubmit={signIn}>
+      <form
+        className="grid grid-cols-1 gap-2"
+        onSubmit={handleSubmit((data) => {
+          signIn(data);
+        })}
+      >
         <h1>Login to your account</h1>
         <input
           className="text-[#000]"
           type="email"
           placeholder="Enter your email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
+          // value={email}
+          // onChange={(e) => setEmail(e.target.value)}
+          {...register('userEmail')}
         ></input>
         <input
           className="text-[#000]"
           type="password"
           placeholder="Enter your password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
+          // value={password}
+          // onChange={(e) => setPassword(e.target.value)}
+          {...register('userPassword')}
         ></input>
         <button
           type="submit"
-          className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+          className="rounded border border-blue-500 bg-transparent px-4 py-2 font-semibold text-blue-700 hover:border-transparent hover:bg-blue-500 hover:text-white"
         >
           Login
         </button>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default SignIn
+export default SignIn;
