e433e8dee785f9497e2641f130df3b1d828dc85e
Ernesto-Tz <ivan.tzompantzi96@gmail.com>
2023-08-16 18:49:23 +0200

    refactor: Cleaning up the Home component
    Cleaning up all the Authentication related components

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,20 +1,11 @@
-import SignIn from '../components/Authentication/SignIn';
-import SignUp from '../components/Authentication/SignUp';
-import AuthDetails from '../components/Authentication/AuthDetails';
-import {useAuthStore} from '../store/authStore'
 import Button from '../components/atoms/Button';
 
 export default function Home() {
-  const user = useAuthStore(state => state.user);
 
   return (
     <>
-      <h1 className="text-3xl md:text-4xl lg:text-6xl">ernesto-chon</h1>
-      <SignIn />
-      <SignUp />
-      <AuthDetails />
+      <h1 className="text-3xl md:text-4xl lg:text-6xl py-5"> Welcome to the ernesto-chon project</h1>
       <Button>Hello button</Button>
-      <h3>User email: {user.email}</h3>
     </>
   );
 }
