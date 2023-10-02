1e9e791e67012f09eaa1121343efd088f055839f
Ernesto-Tz <ivan.tzompantzi96@gmail.com>
2023-10-02 18:55:22 +0200

    style: :lipstick: Update Layout structure and styles

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,35 +1,34 @@
 import { Outlet, useLocation } from 'react-router-dom';
-import { useMemo } from 'react';
+// import { useMemo } from 'react';
 
 import Navbar from './components/organism/Navbar';
-        
+
 export default function Layout() {
-  const { pathname } = useLocation();
+  // const { pathname } = useLocation();
 
-  const color = useMemo(() => {
-    const colorTable = [
-      { path: '/', color: 'from-slate-900 to-slate-950' },
-      { path: '/products', color: 'from-teal-900  to-cyan-950' },
-      { path: '/who-we-are', color: 'from-green-900  to-emerald-950' },
-      {path: '/showcase', color: 'from-base-100 to-base-200 !text-base-content'},
-      { path: '/my-account', color: 'from-blue-600  to-blue-950' },
-    ];
+  // const color = useMemo(() => {
+  //   const colorTable = [
+  //     { path: '/', color: 'from-slate-900 to-slate-950' },
+  //     { path: '/products', color: 'from-teal-900  to-cyan-950' },
+  //     { path: '/who-we-are', color: 'from-green-900  to-emerald-950' },
+  //     { path: '/showcase', color: 'from-base-100 to-base-200 !text-base-content' },
+  //     { path: '/my-account', color: 'from-blue-600  to-blue-950' },
+  //   ];
 
-    const { color } = colorTable.find((object) => object.path === pathname) || {
-      color: 'from-neutral-900  to-neutral-950',
-    };
+  //   const { color } = colorTable.find((object) => object.path === pathname) || {
+  //     color: 'from-neutral-900  to-neutral-950',
+  //   };
 
-    return color;
-  }, [pathname]);
+  //   return color;
+  // }, [pathname]);
 
   return (
-    <div className={`grid place-items-center min-h-screen !bg-gradient-to-bl ${color} text-white`}>
-      <div className="space-y-2">
-        <div className={`mx-4 bg-gradient-to-tr p-16 text-center ${color}`}>
+    <div className={`flex flex-col min-h-screen`}>
+      <Navbar />
+      <div className="space-y-2 flex-1">
+        <div className={`mx-4 p-16 text-center`}>
           <Outlet />
         </div>
-
-        <Navbar />
       </div>
     </div>
   );
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -3,9 +3,13 @@ import Button from '@/components/atoms/Button';
 export default function Home() {
 
   return (
-    <>
-      <h1 className="text-3xl md:text-4xl lg:text-6xl py-5"> Welcome to the ernesto-chon project</h1>
-      <Button>Hello button</Button>
-    </>
+    <div className='grid grid-cols-1'>
+      <div>
+        <h1 className="text-3xl md:text-4xl lg:text-6xl py-5"> Welcome to the ernesto-chon project</h1>
+      </div>
+      <div>
+        <Button>Hello button</Button>
+      </div>
+    </div>
   );
 }
