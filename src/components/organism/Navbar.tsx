51211930db71b7568aeca32af3510c728019e64e
Ernesto-Tz <ivan.tzompantzi96@gmail.com>
2023-10-16 18:10:30 +0200

    refactor: :construction: Include TS syntax

--- src/components/organism/Navbar.tsx
+++ src/components/organism/Navbar.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import Button from '../atoms/Button';
@@ -9,17 +9,21 @@ import NavMenu from '../atoms/NavMenu';
 import Grid from '@/assets/icons/grid.svg?react';
 
 export default function Navbar() {
-  const [showMenu, setShowMenu] = useState(false);
+  const [showMenu, setShowMenu] = useState<boolean>(false);
 
-  const toggleMenu = (menuState) => {
+  const toggleMenu = (menuState: boolean) => {
     setShowMenu(menuState);
   };
+
   return (
     <nav className="relative top-0 w-screen bg-primary-gray">
       <div className="container mx-auto flex items-center justify-center gap-5 py-5">
-        <div className="justify-self-end">
+        <div className="logo">
           <Link to="/">
-            <img src="https://themegrilldemos.com/webshop/wp-content/uploads/sites/165/2021/08/webshop-logo.png" />
+            <img
+              src="https://themegrilldemos.com/webshop/wp-content/uploads/sites/165/2021/08/webshop-logo.png"
+              alt="Logo"
+            />
           </Link>
         </div>
         <div className="hidden w-full max-w-[45rem] md:flex">
@@ -30,13 +34,13 @@ export default function Navbar() {
           <NavShoppingCart />
         </div>
       </div>
-      <div className="flex p-5 md:hidden">
+      <div className="flex justify-center p-5 md:hidden">
         <NavSearch />
       </div>
       <div className="bg-white py-5">
-        <div className="mx-0 flex items-center justify-center gap-5 md:container md:mx-auto">
-          <div className="justify-self-end">
-            <button className="d-btn rounded-none bg-[#ebebec] transition-colors !duration-300 hover:fill-primary hover:text-primary lg:text-lg">
+        <div className="mx-auto flex items-center justify-center gap-5 md:container">
+          <div className="drawer">
+            <button className="d-btn rounded-none bg-secondary-gray transition-colors !duration-300 hover:bg-primary-gray hover:fill-primary hover:text-primary lg:text-lg">
               <Grid height="24px" width="20px" className="mx-2" />
               Browse Categories
             </button>
