11bf1612e6ebbb012f3f978c30d208f6b754d2bc
Ernesto-Tz <ivan.tzompantzi96@gmail.com>
2023-10-31 15:38:23 +0100

    refactor: :construction: Fixes on the Breadcrums
    Change on the logic for the title and path

--- src/components/atoms/BreadcrumsContent.tsx
+++ src/components/atoms/BreadcrumsContent.tsx
@@ -1,25 +1,29 @@
 import React from 'react';
 import ChevronIcon from '@/assets/icons/chevron-right.svg?react';
+import ContentSection from './ContentSection';
 
 export default function BreadcrumsContent() {
-  const currentPath = window.location.pathname;
+  const currentPath = window.location.pathname.slice(1);
+  const title = currentPath.charAt(0).toUpperCase() + currentPath.slice(1);
 
   return (
-    <div className="my-10 flex flex-col bg-secondary-gray">
-      <div className="d-breadcrumbs">
-        <ul>
-          <li className="px-2">
-            <a href="/">Home</a>
-            <ChevronIcon height="18px" className="pl-2" />
-          </li>
-          <li className="px-2">
-            <a href={currentPath}>{currentPath.slice(1)}</a>
-          </li>
-        </ul>
-      </div>
-      <div className="mt-5 text-left font-extrabold">
-        <h2>Shop</h2>
-      </div>
+    <div className="flex flex-col bg-secondary-gray">
+      <ContentSection>
+        <div className="d-breadcrumbs text-xl">
+          <ul>
+            <li className="px-2">
+              <a href="/">Home</a>
+              <ChevronIcon height="18px" className="pl-2" />
+            </li>
+            <li className="px-2">
+              <a href={currentPath}>{currentPath}</a>
+            </li>
+          </ul>
+        </div>
+        <div className="mt-5 text-left font-bold">
+          <h2>{title}</h2>
+        </div>
+      </ContentSection>
     </div>
   );
 }
