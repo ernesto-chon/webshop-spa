959422a401546595635b646cf8e475932f1654b2
Ernesto-Tz <ivan.tzompantzi96@gmail.com>
2023-11-07 17:37:33 +0100

    feat: :sparkles: Create Loader Component

---
+++ src/components/atoms/LoadingBall.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function LoadingBall() {
+  return (
+    <>
+      <span className="d-loading d-loading-ball d-loading-xs"></span>
+      <span className="d-loading d-loading-ball d-loading-sm"></span>
+      <span className="d-loading d-loading-ball d-loading-md"></span>
+      <span className="d-loading d-loading-ball d-loading-lg"></span>
+    </>
+  );
+}
