c91375217b6085ed139d2224dd3b4d20a890da20
Ernesto-Tz <ivan.tzompantzi96@gmail.com>
2023-11-07 17:43:15 +0100

    refactor: :art: Update Product Card
    Includes navigation into the single product view

--- src/components/atoms/ProductCard.tsx
+++ src/components/atoms/ProductCard.tsx
@@ -1,27 +1,31 @@
-import React from 'react';
+import { useNavigate } from 'react-router-dom'
+
 import Button from './Button';
 
 type Props = {
+  id: number
   imgSrc: string;
   title: string;
   price: number;
-  onSelectItem?: () => void;
 };
 
 export default function ProductCard(props: Props) {
+
+  const navigate = useNavigate();
   const options = {
     style: 'decimal',
     minimumFractionDigits: 2,
     maximumFractionDigits: 2,
   };
   const price = props.price.toLocaleString('en-US', options);
+
   return (
-    <div className="d-card w-60 cursor-pointer rounded-md bg-base-100 transition-transform !duration-300 hover:-translate-y-2 hover:shadow-xl xl:w-80">
+    <div className="d-card w-60 cursor-pointer bg-base-100 transition-transform !duration-300 hover:-translate-y-2 hover:shadow-xl xl:w-80" onClick={() => { navigate(`/products/${props.id}`)} }>
       <figure>
-        <img src={props.imgSrc} alt="product" className="max-h-[12rem] rounded-t-lg" />
+        <img src={props.imgSrc} alt="product" className="h-56 object-fill" />
       </figure>
       <div className="d-card-body p-5">
-        <h2 className="d-card-title">{props.title}</h2>
+        <h3 className="d-card-title">{props.title}</h3>
         <p className="text-base text-primary">${price}</p>
         <div className="d-card-actions mt-3 justify-end">
           <Button>Add item</Button>
