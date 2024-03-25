ab06697a1ee1995f285922e81e935374242783b8
Ernesto-Tz <ivan.tzompantzi96@gmail.com>
2024-03-25 17:51:57 +0100

    build: Update on eslinttrc file
    This only formats the file with prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,19 +1,19 @@
 module.exports = {
-  env: { 
-    browser: true, 
-    es2020: true , 
-    node: true
+  env: {
+    browser: true,
+    es2020: true,
+    node: true,
   },
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
-    'plugin:@tanstack/eslint-plugin-query/recommended'
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@tanstack/eslint-plugin-query/recommended',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  plugins: ['react-refresh',"@tanstack/query" ],
+  plugins: ['react-refresh', '@tanstack/query'],
   rules: {
     'react-refresh/only-export-components': 'warn',
   },
-}
+};
